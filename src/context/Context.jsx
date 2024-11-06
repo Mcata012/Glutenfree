1a9e8e1a849dc698cdcecf3bc13670a0e6f57dba
Mcata <98522242+Mcata012@users.noreply.github.com>
2024-11-06 10:38:50 +0100

    placed comments

--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -2,11 +2,15 @@ import { createContext, useState } from "react";
 import { food_list, menu_list } from "../assets/assets";
 import PropTypes from "prop-types";
 
+// lager  produkter og håndeter det i handlekurven
 export const Context = createContext(null);
 
+//prop-types brukes til å validere at props er riktig eller sendt inn.
+//her er cartitem en usestate tilstand som lagrer antall av hver vare i handlekurven.
 const ContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
 
+  // Add to cart, her sjekker vi om varen er lagt inn fra før , om ikke så plusses det med 1
   const addToCart = (itemId) => {
     if (!cartItems[itemId]) {
       setCartItems((prev) => ({ ...prev, [itemId]: 1 }));
@@ -14,10 +18,12 @@ const ContextProvider = (props) => {
       setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
     }
   };
+  //fjerner fra handlekruven
   const removeFromCart = (itemId) => {
     setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
   };
 
+  //beregner totalpris for handlekurven og her henter vi alt fra item, cartimten info , food_list osv
   const getTotalCartAmount = () => {
     let totalAmount = 0;
     for (const item in cartItems) {
@@ -34,7 +40,7 @@ const ContextProvider = (props) => {
   const placeOrder = (deliveryData) => {
     console.log(deliveryData);
   };
-
+  //henter info fra assets med constextvalue
   const contextValue = {
     food_list,
     menu_list,
@@ -45,11 +51,14 @@ const ContextProvider = (props) => {
     placeOrder,
   };
 
+  //constext provider er bare en komponent som omgir andre komponenter
+  //( hadde jeg ikke tatt med den så hadde jeg ikke kunne hentet conxtextvalue)
   return (
     <Context.Provider value={contextValue}>{props.children}</Context.Provider>
   );
 };
 
+// Validerer children at den blir passet inn som en gyldig node
 ContextProvider.propTypes = {
   children: PropTypes.node.isRequired,
 };
